0f01c89f85e52dc3667015f4b66fe25fc12360e7
Marcos Mohar <elcabeca@gmail.com>
2014-12-30 23:56:49 -0600

    added test cases for discount calculator edge cases

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,3 +1,21 @@
 def calculate_discount(item_cost, relative_discount, total_discount):
-  pass
+  
+  if relative_discount > 99.9 or total_discount > 99.9:
+  	raise ValueError("The discount can't be higher than 100%")
+  
+  if item_cost <= 0.0:
+    raise ValueError("You can't discount from zero")
 
+  rd = item_cost * (relative_discount/100)
+  td = (item_cost - rd) * (total_discount/100)
+  rd = item_cost -rd
+
+  return rd - td
+
+
+if __name__ == '__main__':
+	print calculate_discount(100.0, 10.0, 20.0)
+	print calculate_discount(500.0, 50.0, 50.0)
+	print calculate_discount(500.0, 80, 80)
+	print calculate_discount(100.0, 110, 50)
+	print calculate_discount(0, 50.0, 50.0)
--- test_discount_calculator.py
+++ test_discount_calculator.py
@@ -2,8 +2,28 @@ import unittest
 from discount_calculator import calculate_discount
 
 class discountCalculatorTest(unittest.TestCase):
-  pass
+  def testCalculateDiscount(self):
+    discount = calculate_discount(500.0, 50.0, 50.0)
+    
+    self.assertEqual(discount, 125.0)
 
+  def testNoRelativeDiscount(self):
+  	discount = calculate_discount(500.0, 0.0, 50.0)
+  	self.assertEqual(discount, 250.0)
+  	
+
+  def testNoTotalDiscount(self):
+  	discount = calculate_discount(500.0, 50.0, 0.0)
+  	self.assertEqual(discount, 250.0)
+
+  def testAbsentItemCost(self):
+  	with self.assertRaises(ValueError):
+  		discount = calculate_discount(0.0, 50.0, 50.0)
+  	
+
+  def testExcesiveDiscount(self):
+  	with self.assertRaises(ValueError):
+  		discount = calculate_discount(100.0, 120.0, 50.0)
 
 if __name__ == "__main__":
   unittest.main()
