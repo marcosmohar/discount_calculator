7fc32b586cc45a2538fca6ed867aa1c53ae0f383
Marcos Mohar <elcabeca@gmail.com>
2014-12-31 01:21:41 +0000

    initial commit

---
+++ discount_calculator.py
@@ -0,0 +1,3 @@
+def calculate_discount(item_cost, relative_discount, total_discount):
+  pass
+
---
+++ test_discount_calculator.py
@@ -0,0 +1,9 @@
+import unittest
+from discount_calculator import calculate_discount
+
+class discountCalculatorTest(unittest.TestCase):
+  pass
+
+
+if __name__ == "__main__":
+  unittest.main()
